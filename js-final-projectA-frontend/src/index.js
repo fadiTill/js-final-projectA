6bc22a71a42a40aecf0796ea12aba76629b8adcd
github username <github email address>
2020-09-02 21:01:54 -0700

    infos guests/houses cards

--- js-final-projectA-frontend/src/index.js
+++ js-final-projectA-frontend/src/index.js
@@ -11,56 +11,80 @@
 
 
 document.addEventListener("DOMContentLoaded",() => {
+
     
-  getHouses()
+  getGuests()
+
    });
 
 
 
 
-   const getHouses = () => {
- fetch('http://localhost:3000/houses')
- .then(response => response.json())
- .then(houses => {
+//    const getGuests = () => {
+//  fetch('http://localhost:3000/houses')
+//  .then(response => response.json())
+//  .then(houses => {
      
-     let housesHTML = houses.map(function(houses){  
-          return`
-<div class="card">
-<div class="container">
-<p hidden>>${houses.id}</p>
-<button id="house-address"> ${houses.address}</button>
- <ul> id="guest-list"</ul> 
-</div>
-</div>
-      `   
+//      let housesHTML = houses.map(function(houses){  
+//           return`
+// <div class="card">
+// <div class="container">
+// <p>${houses.id}</p>
+// <button id="house-address"> ${houses.address}</button>
+//  <ul> id="guest-list"</ul> 
+// </div>
+// </div>
+//       `   
 
      
 
-     })
+//      })
      
-      document.querySelector("#house-collection").innerHTML+= housesHTML 
- })
-};
+//       document.querySelector("#house-collection").innerHTML+= housesHTML 
+//  })
+// };
 
-    const getGuests = 
+
+//  const getGuests = 
+     // fetch('http://localhost:3000/guests')
+     // .then(response => response.json())
+     //  .then(data => console.log(data))
+
+    const getGuests = () => {
      fetch('http://localhost:3000/guests')
      .then(response => response.json())
-     .then(data => 
-     // .then(data => console.log(data))
-  {
+     .then(data => {
+ 
+//   {
+
+
+     // t.string "name"
+     // t.string "phone_number"
+     // t.string "address"
+     // t.string "email"
      
-     let guestsHTML = data.map(function(guests){  
+     let guestsHTML = data.map(function(guests) {  
           return`
  <div class="card">
 <div class="container">
-<p >${guests.house_id}</p>
+<h2>${guests.house.address}</h2>
+<ul>
+<li>
+<p> ${guests.name}</p>
 <p hidden>${guests.id}</p>
-<button id="house-address"> ${guests.name}</button>
+<p>${guests.phone_number}</p>
+<p>${guests.address}</p>
+<p> ${guests.email}</p>
+</ul>
+<button data-guest-id=${guests.id}>update</button>
 </div>
 </div>
 
 `
- });
+ })
          
  document.querySelector("#guest-collection").innerHTML+= guestsHTML 
-}) 
+})
+     
+    }
+     
