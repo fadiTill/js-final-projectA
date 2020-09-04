54e80e518bc6d2e48d90d65a9755875174ba9ddd
github username <github email address>
2020-09-04 15:19:10 -0700

    create card

--- js-final-projectA-frontend/src/index.js
+++ js-final-projectA-frontend/src/index.js
@@ -76,7 +76,7 @@ document.addEventListener("DOMContentLoaded",() => {
 <p>${guests.address}</p>
 <p> ${guests.email}</p>
 </ul>
-<button data-guest-id=${guests.id}>update</button>
+<button data-guest-id=${guests.id}>delete guest card</button>
 </div>
 </div>
 
@@ -86,5 +86,89 @@ document.addEventListener("DOMContentLoaded",() => {
  document.querySelector("#guest-collection").innerHTML+= guestsHTML 
 })
      
-    }
+    };
+
+
+    // when click on create, the new form appear with input fielsd and submit button
+
+//     const input = document.getElementById('create-guest');
+//    input.addEventListener('click', function(event) {
+//   alert('I was clicked!');
+// });
+
+let guests = []
+
+
+document.getElementById("create-guest").addEventListener('click', (e) => {
+     document.querySelector("#guest-collection").append(generateForm())
+document.querySelector("#new-guest-card").addEventListener("submit", (e))
+// prevent form from being submit and avoid error e.prevent delfault not a function
+ return false; 
+let guest = e.target.elements.newguest.value
+document.querySelector(".guests").append(guest)
+})
+
+
+const generateForm = () => {
+     // const form = document.createElement("form")
+     // const guestInput = document.createElement("input")
+     // const submit = document.createElement("button")
+
+
+
+     // form.id = "new-guest-card"
+
+     // guestInput.id ="new-guest"
+     // guestInput.placeholder = "create-guest"
+     // guestInput.name = "newguest"
+
      
+
+
+     // submit.classList.add("button")
+     // submit.testContent ="create Guest!"
+
+     const guestInput = document.createElement("input")
+    const submit = document.createElement("button")
+
+
+     const form =
+`
+<form>
+<div class="card">
+<div class="container">
+<h2>"guests.house.address"</h2>
+<ul>
+<li>
+<p> ${ guestInput.name }</p>
+<p hidden>${guestInput.id}</p>
+<p>"guests.phone_number"</p>
+<p>"guests.address"</p>
+<p> "guests.email"</p>
+</ul>
+<button data-guest-id="guests.id">delete guest card</button>
+</div>
+</div>
+</form>
+`
+ 
+
+
+
+
+form.id = "new-guest-card"
+
+ guestInput.id ="new-guest"
+//  guestInput.placeholder = "create-guest"
+ guestInput.name = "newguest"
+
+
+
+
+     //  form.appendChild(guestInput)
+     //  form.appendChild(submit)
+
+     
+
+     return form
+}
